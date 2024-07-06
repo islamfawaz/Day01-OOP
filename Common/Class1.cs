3174dba15a4506b4fbaa62813211a1f0736b2b4d
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-07-06 16:27:25 +0300

    UserDefinedType Video2

--- Common/Class1.cs
+++ Common/Class1.cs
@@ -4,18 +4,20 @@
     {
 
     }
-    public interface ICar
-    {
-        void Accelrate();
-    }
-    public class BMCar : ICar
-    {
-        public void Accelrate()
-        { }
-    }
-    public enum Gender
-    {
-        male ,
-        female
-    }
+    #region UserDefinedType Video2
+    //public interface ICar
+    //{
+    //    void Accelrate();
+    //}
+    //public class BMCar : ICar
+    //{
+    //    public void Accelrate()
+    //    { }
+    //}
+    //public enum Gender
+    //{
+    //    male,
+    //    female
+    //} 
+    #endregion
 }
