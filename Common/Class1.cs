cbaca725a14f414d2da339125c95083fd4f58df8
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-07-06 16:24:12 +0300

    Add project files.

---
+++ Common/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace Common
+{
+    public class Class1
+    {
+
+    }
+    public interface ICar
+    {
+        void Accelrate();
+    }
+    public class BMCar : ICar
+    {
+        public void Accelrate()
+        { }
+    }
+    public enum Gender
+    {
+        male ,
+        female
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+namespace Demo
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
